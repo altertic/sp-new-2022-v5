1ff80e7c8f6abbf050e81a342717b4409fa3f2a9
@lterTIC <77030829+altertic@users.noreply.github.com>
2021-12-17 14:31:40 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="contact-page" class="page-wrapper contact-page content-page">
     <site-hero
-      title="Contact Us"
-      subtitle="Example Contact Page"
+      title="Contacts"
+      subtitle="Sport & Collection"
       image="/uploads/contact-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
@@ -11,10 +11,10 @@
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">
-                Hello!
+                Contactez l'équipe
               </p>
               <p class="subtitle">
-                What can I do for you?
+                de Sport et Collection
               </p>
               <figure class="image is-1by1 ">
                 <opti-image
@@ -28,15 +28,17 @@
               <div class="content">
                 <p>
                   <strong>
-                    Edit this page in<code>/pages/contact.vue</code>to fit your
-                    needs.
+                    Vous pouvez contacter l’équipe de Sport et Collection pour tout renseignement, demande d’informations complémentaires, proposition de partenariat etc.
                   </strong>
                 </p>
                 <p>
-                  This form doesn't actually work but would be easy enough to
-                  slap an action on to have it post wherever you'd like. Or put
-                  the netlify attribute on it and have netlify handle all your
-                  form submissions...
+                  Nous recevons de très nombreuses demandes, notamment durant le mois précédent le début de la nouvelle édition de Sport et Collection « 500 Ferrari contre le cancer ». Nous vous prions, par conséquent, de bien vouloir prendre le temps de vérifier que les réponses à vos questions ne se trouvent pas déjà sur notre site. C’est notamment le cas, par exemple, de toutes les informations pratiques relatives aux dates et horaires d’ouverture, aux tarifs, au timing des différents plateaux proposés etc.
+                </p>
+                <p>
+                  Vous trouverez, dans la section "Infos Pratiques" du site, un rappel des principales informations, à jour, relatives à l’édition de Sport & Collection à venir.
+                </p>
+                <p>
+                  Si, après vérification, vous n’avez pas pu trouver de réponses sur le site, n’hésitez pas à nous envoyer un message via le formulaire ci-dessous.
                 </p>
               </div>
             </article>
@@ -50,9 +52,11 @@
       </template>
       <template v-slot:sidebar>
         <h3 class="subtitle is-4">
-          Latest Posts
+          Dernières News
         </h3>
-        <!-- Latest Posts -->
+        <p>
+          Retrouvez-nous au Salon Retromobile : Porte de Versailles à Paris du 2 au 6 février 2022 sur notre stand habituel, Hall 1.
+        </p>
         <posts-grid :per-row="1" :number="2" />
       </template>
     </main-section>
